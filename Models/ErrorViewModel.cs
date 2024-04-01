c591aa9706691e80f81f5b4717c6619786eaafdf
Mlondi <wisemanmlora@gmail.com>
2024-04-01 19:05:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BABERSHOPWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
